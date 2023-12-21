4cd5d65be2d5269c2ec5cbbf3ef18c91bc6c4c42
avalasai <avalasai@gmail.com>
2023-12-21 10:54:22 -0800

    added few changes like await for updatedcontact

--- Controllers/contactController.js
+++ Controllers/contactController.js
@@ -38,13 +38,13 @@ const updateContact = asyncHandler(async (req,res)=>{
         res.status(404);
         throw new Error("Contact not found");
     }
-    const updateContact =  Contact.findByIdAndUpdate(
+    const updatedContact = await Contact.findByIdAndUpdate(
         req.params.id,
         req.body,
         {new : true}
     );
     
-    res.status(200).json(updateContact);
+    res.status(200).json(updatedContact);
 });
 
 //@desc update contacts
